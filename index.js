0cd411ea1e059e761b7d901bda2e5895d18d6e92
Kyle <ktvitale7@gmail.com>
2022-05-06 21:23:34 -0400

    commit tree 7ec84a0c2f3fb8c38fb1202a841f7d79e0fe8b56

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solutions in this file
+function writeCards(namesArray, event){
+    const cards = []
+    for(let i = 0; i < namesArray.length; i++ ){
+        cards.push(`Thank you, ${namesArray[i]}, for the wonderful ${event} gift!`) 
+    }
+    return cards
+}
+
+function countDown(){
+   let countDown = 0
+   while (countDown < 11)
+   console.log(countDown++)
+}
+
+
+
+// const gifts = ["teddy bear", "drone", "doll"];
+
+// function wrapGifts(gifts) {
+//   for (let i = 0; i < gifts.length; i++) {
+//     console.log(`Wrapped ${gifts[i]} and added a bow!`);
+//     debugger;
+//   }
+
+//   return gifts;
+// }
+
+// wrapGifts(gifts);
